60780d0bddb38bef71436b6262f692d944e689f4
KiRiTi Nain <110725426+kirinain@users.noreply.github.com>
2023-12-04 06:54:48 +0530

    Add files via upload

---
+++ 17th_nov.py
@@ -0,0 +1,68 @@
+print("Hello kiti")
+
+'''
+
+print("Hello kiti")
+print("Hello naima")
+print("Hello aaradhya")
+print("hello vipul")
+
+
+'''
+
+a = 3
+print(a)
+b = 15
+print(b)
+a=1.5
+print(a)
+# include content in double inverted commas to make a string that pythin can use.
+
+e = None 
+print(e)
+#None is defined in python
+
+f = "5"
+print(int(f)+1)
+
+g = 5
+print(g)
+
+#typecasting converting one data type into other data type
+#convert string into int data type to add
+ 
+#operators
+
+num1 = 10
+num2 = 2
+
+#Arithmatic operators
+
+print("num1 + num2 is ", num1 + num2)
+print("num1 - num2 is ", num1 - num2)
+print("num1 * num2 is ", num1 * num2)
+print("num1 / num2 is ", num1 / num2)
+print("num1 // num2 is ", num1 // num2) #float division
+
+#Assignment Operators
+
+a = 4
+a += 2
+print(a)
+
+#Comparison Operators
+
+x = 8
+y = 3
+z = 8
+print(x>y)
+print(x<y)
+print(x!=y)
+print(x==z)
+
+#Logical Operators
+print(x==z and x<y)
+print(x==z or x<y)
+print(not(False))
+print(not(True))
+
